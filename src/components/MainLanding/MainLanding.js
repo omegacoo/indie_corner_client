f43697c0196abe0c6139ed0a987eb8e82ac24d6c
omegacoo <bsumser@yahoo.com>
2020-05-17 13:36:05 -0700

    registration and login will both be popups

--- src/components/MainLanding/MainLanding.js
+++ src/components/MainLanding/MainLanding.js
@@ -6,18 +6,56 @@ import './MainLanding.css';
 export default class MainLanding extends React.Component {
     state = {
         loggingIn: false,
+        registering: false,
         error: null,
     }
 
     handleLoginClick = () => {
-        console.log('login clicked!');
         this.setState({
             loggingIn: !this.state.loggingIn
         });
     }
 
+    handleRegisterClick = () => {
+        this.setState({
+            registering: !this.state.registering
+        });
+    }
+
     renderLogin = () => {
         if(this.state.loggingIn){
+            return(
+                <div className='popupContainer'>
+                    <span className='popup'>
+                        <i className='closePopup far fa-times-circle' />
+                        <form 
+                            className='login_form'
+                            onSubmit={this.handleLoginSubmit}
+                        >
+                            <label htmlFor='login_username'>Username: </label>
+                            <input 
+                                value={this.state.username}
+                                onChange={this.handleUsernameChange}
+                                type='text'
+                                autoFocus
+                            />
+                            <label htmlFor='login_password'>Password: </label>
+                            <input
+                                value={this.state.password}
+                                onChange={this.handlePasswordChange}
+                                type='password'
+                            />
+                            {this.state.error ? <h3 id='Login_error'>{this.state.error}</h3> : null}
+                            <button className='login_button' type='submit'>login</button>
+                        </form>
+                    </span>
+                </div>
+            )
+        }
+    }
+
+    renderRegistration = () => {
+        if(this.state.registering){
             return(
                 <div className='popupContainer'>
                     <span className='popup'>
@@ -62,12 +100,12 @@ export default class MainLanding extends React.Component {
                     Log in
                 </div>
 
-                <Link 
+                <div 
                     className='MainLanding-register-button'
-                    to={'/register'}
+                    onClick={ this.handleRegisterClick }
                 >
                     Register
-                </Link>
+                </div>
 
                 <Link 
                     className='MainLanding-anonymous-button'
@@ -77,6 +115,7 @@ export default class MainLanding extends React.Component {
                 </Link>
 
                 { this.renderLogin() }
+                { this.renderRegistration() }
             </div>
         );
     };
