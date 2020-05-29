ebb507795baa0ac32a877731d116c96f74374332
omegacoo <bsumser@yahoo.com>
2020-05-28 20:19:06 -0700

    put correct api_endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
     // API_ENDPOINT: 'http://localhost:8000'
-    API_ENDPOINT: 'postgres://kqyynnyasepguc:1c5eb7d5de8748dde21e02d7607388a3871184d7c953dfa4004ed7e36149c777@ec2-54-165-36-134.compute-1.amazonaws.com:5432/dc7v21admvl7ub'
+    API_ENDPOINT: 'https://boiling-bastion-93233.herokuapp.com/'
 };
